1a673ca97cc7b6ca2ceddf1da6cd89dd4d653991
Brian Woodward <brian.woodward@gmail.com>
2015-09-10 00:55:36 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * templates-indexer <https://github.com/doowb/templates-indexer>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
